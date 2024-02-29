68d2e9d7d0c6c142914399d76b9c7d2ff18abc6d
mohit7456 <115362344+mohit7456@users.noreply.github.com>
2024-02-29 12:22:19 +0000

    update

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -46,3 +46,18 @@ let myObj = {
 const myFunction = function(){
     console.log("Hello world");
 }
+
+// Return type of variables in JavaScript
+// 1) Primitive Datatypes
+//        Number => number
+//        String  => string
+//        Boolean  => boolean
+//        null  => object
+//        undefined  =>  undefined
+//        Symbol  =>  symbol
+//        BigInt  =>  bigint
+
+// 2) Non-primitive Datatypes
+//        Arrays  =>  object
+//        Function  =>  function
+//        Object  =>  object
