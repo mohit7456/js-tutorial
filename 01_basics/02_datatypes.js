293fae22e53ebf26adf32493fff5bbad2833f9fc
mohit7456 <115362344+mohit7456@users.noreply.github.com>
2024-02-29 03:49:37 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict";  // treat all JS code as newer syntax
+
+// alert(3+3)  // we are using nodeJS , not browser
+
+console.log(3 + 
+    3)         // code readability should be high
+
+
+
+let name = "Mohit"
+let age = 29
+let isLoggenIn = false
+
+       // Datatypes
+       // number => 2 power 53
+       // bigint
+       // string => ""
+       // boolean => true/false
+       // null => standalone value
+       // undefined => value is not assigned to the variable
+       // symbol => find uniqueness
+
+       
+       // Object
+
+console.log(typeof "Mohit") // type => string
+console.log(typeof age)    // type => number
+console.log(typeof null)  // type => object
+console.log(typeof undefined) // type => undefined
